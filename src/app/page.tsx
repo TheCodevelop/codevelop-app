83d9108244823a0c547998a3c2df956f4b49b8e6
TheCodevelop <sebastiansalazarp27@gmail.com>
2023-12-06 13:18:24 -0500

    added some gradient stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
 import Image from "next/image";
-
+// This is the home page
 export default function Home() {
   return (
-    <main className="gradient" style={{ height: "900px" }}>
-      Hello World
+    <main className="gradient" style={{ height: "53vw" }}>
+      <p
+        style={{
+          color: "white",
+          position: "absolute",
+          top: "150px",
+          left: "150px",
+          fontSize: "30px",
+        }}
+      >
+        {" "}
+        CoDevelop
+      </p>
     </main>
   );
 }
