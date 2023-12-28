d0b8e2969d86ab923d6c8f6c77a5ce901acbb08a
Sebastian Salazar <sebastian@Sebastians-MacBook-Pro.local>
2023-12-28 11:14:25 -0500

    added lodash and used throttle to stop animations on window resize

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,26 @@
+"use client";
 import Link from "next/link";
 import Image from "next/image";
 import styles from "./navbar.module.scss";
+import { useEffect, useState } from "react";
+import throttle from "lodash/throttle";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [resizing, setResizing] = useState(false);
+
+  useEffect(() => {
+    // This entire block of code serves the purpose  of freezing the css opacity animation of the
+    // navbar when the window is resizing by manipulating the value of the "data-resizing" attribute
+    const resizeState = throttle(() => {
+      setResizing(true);
+      setTimeout(() => {
+        setResizing(false);
+      }, 300);
+    }, 301);
+    window.addEventListener("resize", resizeState);
+    return () => window.removeEventListener("resize", resizeState);
+  }, []);
   return (
     <header className="py-24 text-2xl flex justify-between">
       <Link href="/">
@@ -17,11 +35,18 @@ const Navbar = () => {
       <button
         className={`${styles.mobile_nav_toggle}`}
         aria-controls="primary-navigation"
-        aria-expanded="false"
+        aria-expanded={isOpen}
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
       >
         <span className="sr-only">Menu</span>
       </button>
-      <nav className={`${styles.primary_navigation} flex gap-8 self-end`}>
+      <nav
+        className={`${styles.primary_navigation} flex gap-8 self-end`}
+        data-visible={isOpen}
+        data-resizing={resizing}
+      >
         <Link className={`${styles.nav_link}`} href="/">
           Our Work
         </Link>
