5f71e70a354442659796c3c5aa09084672ac1bfc
Sebastian Salazar <sebastian@Sebastians-MacBook-Pro.local>
2023-12-28 14:58:56 -0500

    make header stick at different sizes when nav is open

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Navbar from "@/components/navbar";
+import { useState } from "react";
 export default function Home() {
   return (
     <main className="main-content">
       <Navbar />
+      <div>Yo</div>
     </main>
   );
 }
