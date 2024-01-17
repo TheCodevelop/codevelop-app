c656e320ff69512cd3b33a97a80a8d8d573bde54
TheCodevelop <sebastiansalazarp27@gmail.com>
2024-01-16 19:48:16 -0500

    responsive text and title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,20 +46,15 @@ export default function Page() {
         </div>
       </div>
       <div className={`${styles.section} pt-24 pb-48`}>
-        <p data-visible={isOpen} className="pb-8 text-5xl font-semibold">
+        <p data-visible={isOpen} className={styles.responsive_title}>
           Our Mission
         </p>
-        <p className="pb-8 text-2xl">
-          At Codevelop, we are a forward-thinking software development company,
-          specializing in crafting custom digital solutions for the modern
-          business. Our mission is to empower the world’s most ambitious
-          companies and brands with cutting-edge technology and innovative
-          software. Our expertise lies in blending strategy, design, and
-          advanced technology to deliver not just software, but transformative
-          digital experiences. Through a commitment to excellence and a passion
-          for digital innovation, we at Codevelop strive to exceed expectations,
-          ensuring that our clients and their customers benefit from the
-          exceptional results we deliver.
+        <p className={styles.responsive_text}>
+          Codevelop specializes in custom digital solutions, combining strategy,
+          design, and advanced technology to deliver transformative experiences.
+          Our mission is on empowering businesses with cutting-edge software,
+          driven by a commitment to innovation and excellence. We aim to exceed
+          expectations and ensure client success.
         </p>
         <Button href="/" text="View Our Work"></Button>
       </div>
