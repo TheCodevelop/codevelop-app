e878de06c0b4e3d19c9bad14691788f5a583083d
TheCodevelop <sebastiansalazarp27@gmail.com>
2024-02-13 15:15:50 -0500

    changes to fix chrome bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Quicksand } from "next/font/google";
 import "./globals.scss";
 import Navbar from "@/components/navbar";
@@ -9,6 +9,13 @@ const quicksand = Quicksand({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Codevelop",
   description: "Main website for Codevelop, a Software Development Company",
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 export default function RootLayout({
